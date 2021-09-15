509288f2b0f203d3cc0aea2320be924b8e825591
behuFTP <benjamin.dals.hughes@gmail.com>
2021-09-15 22:40:10 +0200

    asd

--- main.js
+++ main.js
@@ -1,41 +1,97 @@
-const today = new Date();
+//http://xmlopen.rejseplanen.dk/bin/rest.exe/location?input=m%C3%A5l%C3%B8v&format=json
 
-const numberOfMinutesToStation = 8;
+async function getDeparturesFromStation(stationId) {
+  const today = new Date();
 
-const dayInMonth = today.getDate();
-const month = today.getMonth() + 1;
-const yearShort = `${today.getFullYear()}`.substring(2, 4);
-const hours = today.getHours();
-const minutes = today.getMinutes();
+  const dayInMonth = today.getDate();
+  const month = today.getMonth() + 1;
+  const yearShort = `${today.getFullYear()}`.substring(2, 4);
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
 
-let trainText = "";
-fetch(
-  `https://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?id=008600709&date=${dayInMonth}.${month}.${yearShort}&time=${hours}:${minutes}&format=json`
-)
-  .then((Response) => Response.json())
-  .then((data) => {
-    const firstDepartures = data["DepartureBoard"]["Departure"];
-    const trainDepartures = firstDepartures.filter(
-      (departure) => departure.type === "S"
+  return fetch(
+    `https://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?id=${stationId}&date=${dayInMonth}.${month}.${yearShort}&time=${hours}:${minutes}&format=json`
+  )
+    .then((Response) => Response.json())
+    .then((data) => {
+      return data["DepartureBoard"]["Departure"];
+    });
+}
+
+async function main() {
+  const numberOfMinutesToStation = 8;
+  const stationTextMåløv = await getStringFromStation(
+    "008600709",
+    "S",
+    "1",
+    "Måløv",
+    "København",
+    numberOfMinutesToStation
+  );
+  document.querySelector(".måløv").innerHTML = stationTextMåløv;
+
+  const stationTextVesterport = await getStringFromStation(
+    "008600645",
+    "S",
+    "4",
+    "Vesterport",
+    "Måløv",
+    numberOfMinutesToStation
+  );
+  document.querySelector(".vesterport").innerHTML = stationTextVesterport;
+}
+
+main();
+
+function getSubtractedDate(date) {}
+
+function getDateFromDepartureString(departureString) {
+  const now = new Date();
+  now.setHours(departureString.time.substring(0, 2));
+  now.setMinutes(departureString.time.substring(3, 5));
+
+  return now;
+}
+
+async function getStringFromStation(
+  stationId,
+  transportType,
+  track,
+  station,
+  towards,
+  numberOfMinutesToStation
+) {
+  let trainText = "";
+
+  const firstDepartures = await getDeparturesFromStation(stationId);
+
+  const trainDepartures = firstDepartures.filter(
+    (departure) => departure.type === transportType
+  );
+  console.log(trainDepartures);
+
+  const trainDeparturesTowardsCopenhagen = firstDepartures
+    .filter((departure) => departure.rtTrack === track)
+    .slice(0, 4);
+
+  trainText += `De næste tog fra ${station} mod ${towards} kører kl<br>`;
+  trainDeparturesTowardsCopenhagen.forEach((departure) => {
+    const dateFromDeparture = getDateFromDepartureString(departure);
+
+    const dateSubtractedTimeToStation = new Date(
+      dateFromDeparture.setMinutes(
+        dateFromDeparture.getMinutes() - numberOfMinutesToStation
+      )
     );
 
-    const trainDeparturesTowardsCopenhagen = firstDepartures
-      .filter((departure) => departure.rtTrack === "1")
-      .slice(0, 4);
-    trainText += "The next trains from Måløv towards Copenhagen leaves a<br>";
-    trainDeparturesTowardsCopenhagen.forEach((departure) => {
-      const trainDate = new Date();
-      trainDate.setHours(departure.time.substring(0, 2));
-      trainDate.setMinutes(departure.time.substring(3, 5));
-      trainDate.setMinutes(trainDate.getMinutes() - numberOfMinutesToStation);
-
+    const leavesAfterNow =
+      dateSubtractedTimeToStation.getTime() > new Date().getTime();
+    if (leavesAfterNow) {
       trainText += `- ${
         departure.time
-      } leave from home at ${trainDate.getHours()}:${trainDate.getMinutes()}<br>`;
-    });
-    console.log(trainDeparturesTowardsCopenhagen);
-    console.log(trainText);
-    document.querySelector("div").innerHTML = trainText;
+      }. Kør hjemmefra kl ${dateSubtractedTimeToStation.getHours()}:${dateSubtractedTimeToStation.getMinutes()}<br>`;
+    }
   });
 
-//http://xmlopen.rejseplanen.dk/bin/rest.exe/location?input=m%C3%A5l%C3%B8v&format=json
+  return trainText;
+}
